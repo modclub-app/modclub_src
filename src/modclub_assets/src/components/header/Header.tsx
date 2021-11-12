95de3957022e04fa8ca124b69da0013a06eb7b46
Taylor Chance <taylor@puffdigital.com>
2021-11-12 14:38:23 -0500

    cleanup landing page

--- src/modclub_assets/src/components/header/Header.tsx
+++ src/modclub_assets/src/components/header/Header.tsx
@@ -5,13 +5,43 @@ import LoginSignup from "./LoginSignup";
 import Hamburger from "./Hamburger";
 export default function Header() {
   return (
+    <nav className="navbar has-background-black py-5" role="navigation" aria-label="main navigation">
+      <div className="container">
+        <div className="navbar-brand">
+          <Logo />
+          {/* <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a> */}
+        </div>
 
-    <div className="Outer">
-      <div className="Header">
-        <Logo />
-        <MenuItems />
-        <Hamburger />
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-end">
+            <a href="#developers" className="navbar-item">
+              Developers
+            </a>
+            <a href="#tokenomics" className="navbar-item">
+              Tokenomics
+            </a>
+            <a href="#team" className="navbar-item">
+              Team
+            </a>
+            <a href="#roadmap" className="navbar-item">
+              Roadmap
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
+
+
+    // <div className="Outer">
+    //   <div className="Header">
+    //     <Logo />
+    //     <MenuItems />
+    //     <Hamburger />
+    //   </div>
+    // </div>
   )
 }
