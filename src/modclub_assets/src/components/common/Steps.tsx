257aa4f4a39248ae4d2cabeddac8a084a4a6a86c
Taylor Chance <taylor@puffdigital.com>
2021-11-30 16:34:44 -0500

    steps pt 1

---
+++ src/modclub_assets/src/components/common/Steps.tsx
@@ -0,0 +1,46 @@
+
+import React from 'react';
+import "./Steps.scss";
+
+const StepContext = React.createContext({});
+const StepProvider = StepContext.Provider;
+const StepConsumer = StepContext.Consumer;
+
+const Step = ({ id, details }) => {  
+  return (
+    <StepConsumer>
+      {context => {
+        console.log('context', context)
+        console.log('id', id)
+
+        return (
+          <li className={`step-item
+            ${context === Number(id) ? "is-active" : ""}
+            ${context > Number(id) ? "is-completed" : ""}
+          `}>
+            <div className="step-marker">
+              {id}
+            </div>
+            {details &&
+              <div className="step-details">
+                <p className="step-title">{details}</p>
+              </div>
+            }
+          </li>
+        )
+      }}
+    </StepConsumer>
+  )
+};
+
+const Steps = ({ activeStep, children }) => {  
+  return (
+    <StepProvider value={activeStep}>
+      <ul className="steps">
+        {children}
+      </ul>
+    </StepProvider>
+  )
+};
+
+export { Steps, Step };
