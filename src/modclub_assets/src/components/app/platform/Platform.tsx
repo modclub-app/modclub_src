4fa40264b2708db591820798e6d2fbfd381c806f
Taylor Chance <taylor@puffdigital.com>
2021-12-02 13:47:49 -0500

    more responsivness

--- src/modclub_assets/src/components/app/platform/Platform.tsx
+++ src/modclub_assets/src/components/app/platform/Platform.tsx
@@ -7,24 +7,30 @@ import { Columns, Card, Level, Heading, Icon } from "react-bulma-components";
 
 const GradientBox = ({ children, title, showToken = true }) => {
   return (
-    <Card className="has-gradient is-fullheight">
-      <Card.Content className="is-fullheight" style={{ padding: "20% 10%" }}>
-        <Heading subtitle size={6} className="has-text-silver">
-          {title}
-        </Heading>
-        <Heading size={1} className="is-flex is-align-items-center" style={{ lineHeight: 1, whiteSpace: "nowrap" }}>
-          {children}
-          {showToken &&
-            <span className="is-size-5 ml-1">
-              MOD
-              <span className="is-block has-text-weight-light">
-                token
+    <Columns.Column
+      tablet={{ size: 4 }}
+      desktop={{ size: 6 }}
+      style={{ padding: ".75rem" }}
+    >
+      <Card className="has-gradient is-fullheight">
+        <Card.Content className="is-fullheight" style={{ padding: "20% 10%" }}>
+          <Heading subtitle size={6} className="has-text-silver">
+            {title}
+          </Heading>
+          <Heading size={1} className="is-flex is-align-items-center" style={{ lineHeight: 1, whiteSpace: "nowrap" }}>
+            {children}
+            {showToken &&
+              <span className="is-size-5 ml-1">
+                MOD
+                <span className="is-block has-text-weight-light">
+                  token
+                </span>
               </span>
-            </span>
-          }
-        </Heading>
-      </Card.Content>
-    </Card>
+            }
+          </Heading>
+        </Card.Content>
+      </Card>
+    </Columns.Column>
   );
 };
 
@@ -90,22 +96,16 @@ export default function Platform ({ providerId }: { providerId: Principal }) {
       </Card.Content>
     </Card>
 
-    <Columns>
-      <Columns.Column size={6}>
-        <GradientBox title="Rq Stake" showToken={false}>
-          {loading ? <div className="loader is-loading"></div> : Number(content.settings.minVotes)}
-        </GradientBox>
-      </Columns.Column>
-      <Columns.Column size={6}>
-        <GradientBox title="Reward">
-          5
-        </GradientBox>
-      </Columns.Column>
-      {/* <Columns.Column size={6}>
-        <GradientBox title="Partner Rewards">
-          5
-        </GradientBox>
-      </Columns.Column> */}
+    <Columns style={{ margin: "-.75rem" }}>
+      <GradientBox title="Rq Stake" showToken={false}>
+        {loading ? <div className="loader is-loading"></div> : Number(content.settings.minVotes)}
+      </GradientBox>
+      <GradientBox title="Reward">
+        5
+      </GradientBox>
+      {/* <GradientBox title="Partner Rewards">
+        5
+      </GradientBox> */}
     </Columns>
   </>
   )
