f42d700ed32c06621e10d423244884c2fc969773
Taylor Chance <taylor@puffdigital.com>
2022-01-06 14:11:42 -0500

    api call working

--- src/modclub_assets/src/components/auth/new_profile/CapturePicture.tsx
+++ src/modclub_assets/src/components/auth/new_profile/CapturePicture.tsx
@@ -2,55 +2,154 @@ import { useRef, useState } from "react";
 import { useHistory, Link } from "react-router-dom";
 import { Heading, Button, Icon } from "react-bulma-components";
 import { WebcamWrapper } from "./Webcam"
-import { submitChallengeData } from '../../../utils/api';
+// import { submitChallengeData } from '../../../utils/api';
+import { b64toBlob, processAndUploadChunk } from "../../../utils/util";
+
+const MAX_CHUNK_SIZE = 1024 * 500;
+
 
 export default function CapturePicture() {
+  const [file, setFile] = useState<FileReaderInfo>({
+    name: '',
+    type: '',
+    size: 0,
+    blob: new Blob(),
+    width: 0,
+    file: 0,
+    height: 0
+  });
+
+  interface FileReaderInfo {
+    name: string;
+    type: string;
+    size: number;
+    blob: Blob;
+    width: number;
+    file: number;
+    height: number;
+  }
+
+  const handleFileChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // @ts-ignore
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      if (reader.result === null) {
+        throw new Error('file empty...');
+      }
+      let encoded = reader.result.toString().replace(/^data:(.*,)?/, '');
+      if ((encoded.length % 4) > 0) {
+        encoded += '='.repeat(4 - (encoded.length % 4));
+      }
+      const blob = b64toBlob(encoded, file.type);
+      const fileInfo: FileReaderInfo = {
+        name: file.name,
+        type: file.type,
+        size: file.size,
+        blob: blob,
+        file: file,
+        width: file.width,
+        height: file.height
+      };
+      console.log("fileInfo", fileInfo);
+      setFile(fileInfo);
+    };
+};
+
+
+
+
   const history = useHistory();
   const inputFile = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
 
-  const handleFileChange = (e) => {
-    const { files } = e.target;
-    if (files.length > 0) {
-      const f = files[0];
-      const reader = new FileReader();
-      reader.onload = function (evt) {
-        console.log("evt", evt)
-        const data = typeof evt.target.result == "string" ? evt.target.result : null;
-        setImgSrc(data);
-      };
-      reader.readAsDataURL(f);
-    }
-  };
+  // const handleFileChange = (e) => {
+  //   const { files } = e.target;
+  //   if (files.length > 0) {
+  //     const f = files[0];
+  //     const reader = new FileReader();
+  //     reader.onload = function (evt) {
+  //       console.log("evt", evt)
+  //       const data = typeof evt.target.result == "string" ? evt.target.result : null;
+  //       setImgSrc(data);
+  //     };
+  //     reader.readAsDataURL(f);
+  //   }
+  // };
 
-  const dataURLtoBlob = (dataurl) => {
-    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-    while(n--){
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new Blob([u8arr], {type:mime});
-  }
+  // const dataURLtoBlob = (dataurl) => {
+  //   var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+  //     bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+  //   while(n--){
+  //     u8arr[n] = bstr.charCodeAt(n);
+  //   }
+  //   return new Blob([u8arr], {type:mime});
+  // }
+
+  // const submit = async () => {
+  //   const blob = dataURLtoBlob(imgSrc)
+  //   console.log("submit blob", blob)
+
+  //   const res = await submitChallengeData({
+  //     challengeId: "challenge-profile-pic",
+  //     // challengeDataBlob: [blob],
+  //     challengeDataBlob: [file],
+  //     userName: [],
+  //     email: [],
+  //     fullName: [],
+  //     aboutUser: [],
+  //     offset: BigInt(1),
+  //     numOfChunks: BigInt(1),
+  //     mimeType: blob.type,
+  //     dataSize: BigInt(blob.size),
+  //   });
+  //   console.log("res", res);
+  //   history.push("/signup2/3")
+  // }
+
+  // const processAndUploadChunk = async (
+  //   blob: Blob,
+  //   byteStart: number,
+  //   chunk: number,
+  //   fileSize: number,
+  //   fileExtension: string
+  // ) : Promise<any> => {
+  //   const blobSlice = blob.slice(
+  //     byteStart,
+  //     Math.min(Number(fileSize), byteStart + MAX_CHUNK_SIZE),
+  //     blob.type,
+  //   );
+   
+  //   const bsf = await blobSlice.arrayBuffer();
+  //   console.log("bsf", bsf);
+
+  //   const res = await submitChallengeData({
+  //     challengeId: "challenge-profile-pic",
+  //     challengeDataBlob: [encodeArrayBuffer(bsf)],
+  //     userName: [],
+  //     email: [],
+  //     fullName: [],
+  //     aboutUser: [],
+  //     offset: BigInt(chunk),
+  //     numOfChunks: BigInt(Number(Math.ceil(file.size / MAX_CHUNK_SIZE))),
+  //     mimeType: fileExtension,
+  //     dataSize: BigInt(fileSize),
+  //   });
+
+  //   console.log("res", res)
+  // }
 
   const submit = async () => {
-    const blob = dataURLtoBlob(imgSrc)
-    console.log("submit blob", blob)
-
-    const res = await submitChallengeData({
-      challengeId: "challenge-profile-pic",
-      // challengeDataBlob: [blob],
-      challengeDataBlob: [imgSrc],
-      userName: [],
-      email: [],
-      fullName: [],
-      aboutUser: [],
-      offset: BigInt(1),
-      numOfChunks: BigInt(1),
-      mimeType: blob.type,
-      dataSize: BigInt(blob.size),
-    });
-    console.log("res", res);
-    // history.push("/signup2/3")
+    const blob = file.blob;
+    const putChunkPromises: Promise<undefined>[] = [];
+    let chunk = 1;
+    for (let byteStart = 0; byteStart < blob.size; byteStart += MAX_CHUNK_SIZE, chunk++ ) {
+      putChunkPromises.push(
+        processAndUploadChunk("challenge-profile-pic", blob, byteStart, chunk, file.size, file.type)
+      );
+    }
+    await Promise.all(putChunkPromises);
   }
 
   return (
@@ -83,10 +182,10 @@ export default function CapturePicture() {
       />
 
       <Button.Group align="right" className="mt-4">
-        <Link to="/app/" className="button is-black" disabled={!imgSrc}>
+        <Link to="/app/" className="button is-black" disabled={!file}>
           Cancel
         </Link>
-        <Button color="primary" disabled={!imgSrc} onClick={submit}>
+        <Button color="primary" disabled={!file} onClick={submit}>
           Next
         </Button>
       </Button.Group>
--- src/modclub_assets/src/utils/util.ts
+++ src/modclub_assets/src/utils/util.ts
@@ -2,6 +2,126 @@ import { Optional } from "./api";
 import { ImageData, Profile } from "./types";
 import { formatDistanceStrict, isSameDay, format } from "date-fns";
 
+import { submitChallengeData } from "./api";
+
+// import { FileExtension, FileInfo } from './declarations/backend/backend.did';
+
+
+// const getReverseFileExtension = (type: { string: null }) : string => {
+export function getReverseFileExtension(type: { string: null }): string {
+  switch(Object.keys(type)[0]) {
+    case 'jpeg':
+      return  'image/jpeg';
+    case 'gif':
+      return  'image/gif'; 
+    case 'jpg':
+      return  'image/jpg';       
+    case 'png':
+      return  'image/png';
+    case 'svg':
+      return  'image/svg';
+    case 'avi':
+      return  'video/avi';
+    case 'mp4':
+      return  'video/mp4';
+    case 'aac':
+      return  'video/aac';
+    case 'wav':
+      return  'audio/wav';
+    case 'mp3':
+      return  'audio/mp3';                                                                                                              
+    default :
+    return "";
+  }
+};
+
+export function getFileExtension(type: string): any | null {
+  switch(type) {
+    case 'image/jpeg':
+      return { 'jpeg' : null };
+    case 'image/gif':
+      return { 'gif' : null };
+    case 'image/jpg':
+      return { 'jpg' : null };
+    case 'image/png':
+      return { 'png' : null };          
+    case 'image/svg':
+      return { 'svg' : null };          
+    case 'video/avi':
+      return { 'avi' : null };                            
+    case 'video/aac':
+      return { 'aac' : null };
+    case 'video/mp4':
+      return { 'mp4' : null };        
+    case 'audio/wav':
+      return { 'wav' : null };                         
+    case 'audio/mp3':
+      return { 'mp3' : null };
+    default :
+    return null;
+  }
+};
+
+export function b64toBlob(b64Data: string, contentType = '', sliceSize = 512) {
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+  const blob = new Blob(byteArrays, { type: contentType } );
+  return blob;
+}
+
+const MAX_CHUNK_SIZE = 1024 * 500;
+
+export async function processAndUploadChunk(
+  challengeId: string,
+  blob: Blob,
+  byteStart: number,
+  chunk: number,
+  fileSize: number,
+  fileExtension: string
+) : Promise<any> {
+  const blobSlice = blob.slice(
+    byteStart,
+    Math.min(Number(fileSize), byteStart + MAX_CHUNK_SIZE),
+    blob.type,
+  );
+ 
+  const bsf = await blobSlice.arrayBuffer();
+
+  const res = await submitChallengeData({
+    challengeId: challengeId,
+    challengeDataBlob: [encodeArrayBuffer(bsf)],
+    userName: [],
+    email: [],
+    fullName: [],
+    aboutUser: [],
+    offset: BigInt(chunk),
+    numOfChunks: BigInt(Number(Math.ceil(fileSize / MAX_CHUNK_SIZE))),
+    mimeType: fileExtension,
+    dataSize: BigInt(fileSize),
+  });
+  console.log("res", res)
+}
+
+
+
+
+
+
+
+
+
 export async function convertImage(imageData: ImageData): Promise<number[]> {
   return new Promise((resolve) => {
     const image = new Image();
