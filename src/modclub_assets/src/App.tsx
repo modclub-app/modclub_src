40829e1d2e85937ef5a81c971f9bdfcccce3196b
niteshudhani <95778087+niteshudhani@users.noreply.github.com>
2022-02-03 10:17:34 -0500

    MOD-90: Using keys owned by Raheel

--- src/modclub_assets/src/App.tsx
+++ src/modclub_assets/src/App.tsx
@@ -20,9 +20,9 @@ export default function App() {
 
   useEffect(() => {
     if (process.env.NODE_ENV === "production") {
-      Usergeek.init({apiKey: "01EC01B48E8C9C81CAE9E94839D511FD"})
+      Usergeek.init({apiKey: "01C001B685B0F32CFCCEEBAA4C97E390"})
     } else {
-      Usergeek.init({apiKey: "01EC01B48E8C9C81CAE9E94839D511FD", host: "https://ljyte-qiaaa-aaaah-qaiva-cai.raw.ic0.app"})
+      Usergeek.init({apiKey: "012701B7CC044FBD096F55C8DAA5413E", host: "https://ljyte-qiaaa-aaaah-qaiva-cai.raw.ic0.app"})
     }
   }, []);
 
