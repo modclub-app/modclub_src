69135e4da859d61b13b0fe77561f4072a1622363
Taylor Chance <taylor@puffdigital.com>
2022-03-18 12:45:12 -0400

    MOD-143

--- src/modclub_assets/src/components/app/poh/UserVideo.tsx
+++ src/modclub_assets/src/components/app/poh/UserVideo.tsx
@@ -19,12 +19,24 @@ export default function UserVideo({ data } : { data: PohTaskData }) {
 
   return (
     <Card.Content>
-      {videoObject &&
+      {videoObject ? (
         <video width="100%" height="auto" controls>
           <source src={videoObject} />
           Your browser does not support the video tag.
         </video>
-      }
+      ) : (
+        <figure className="image is-16by9">
+          <div className="loader is-loading p-6"
+           style={{
+             position: "absolute",
+             top: 0,
+             left: 0,
+             right: 0,
+             bottom: 0
+            }}
+          />
+        </figure>
+      )}
 
       <Card className="mt-5">
         <Card.Content className="columns is-multiline">
