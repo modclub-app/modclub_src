4a1a5f20c5810c94d6b6019d48d0226c6bb7fa01
Taylor Chance <taylor@puffdigital.com>
2022-04-05 16:06:21 -0400

    progress

--- src/modclub_assets/src/components/auth/poh/Webcam.tsx
+++ src/modclub_assets/src/components/auth/poh/Webcam.tsx
@@ -3,6 +3,7 @@ import { useRef, useState, useCallback } from "react";
 import { Button, Icon } from "react-bulma-components";
 import Webcam from "react-webcam";
 import { b64toBlob, getFileExtension } from "../../../utils/util";
+import Cropper from "react-easy-crop";
 
 export function CaptureButton({
   icon,
@@ -69,10 +70,10 @@ export function SaveButton({ file }) {
 export function WebcamWrapper({ setFile, file }) {
   const [loading, setLoading] = useState<boolean>(true);
   const webcamRef = useRef(null);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState();
 
   const captureWebcam = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
-    console.log("imageSrc", imageSrc);
 
     let encoded = imageSrc.toString().replace(/^data:(.*,)?/, '');
     if ((encoded.length % 4) > 0) {
@@ -90,6 +91,34 @@ export function WebcamWrapper({ setFile, file }) {
     setFile(fileInfo);
   }, [webcamRef, setFile]);
 
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+
+  const cropImage = (imgUri, width = 400, height = 300, xstart = 0, ystart = 0, callback) => {
+    try {
+      let resize_canvas = document.createElement('canvas');
+      let orig_src = new Image();
+      orig_src.src = imgUri;
+      orig_src.onload = function () {
+        resize_canvas.width = width;
+        resize_canvas.height = height;
+        let cnv = resize_canvas.getContext('2d');
+        cnv.drawImage(orig_src, xstart, ystart, width, height, 0, 0, width, height);
+        let newimgUri = resize_canvas.toDataURL("image/jpeg").toString();
+        callback(newimgUri);
+      }
+    }
+    catch (e) {
+      console.log("Couldn't crop image due to", e);
+      callback(imgUri);
+    }
+  }
+
+  const onCropComplete = useCallback((croppedAreaPixels) => {
+    console.log("croppedAreaPixels", croppedAreaPixels);
+    setCroppedAreaPixels(croppedAreaPixels);
+  }, []);
+
   const clearImage = () => {
     setLoading(true);
     setFile({
@@ -97,6 +126,25 @@ export function WebcamWrapper({ setFile, file }) {
       size: 0,
       blob: new Blob(),
       data: null
+    });
+  }
+
+  const doCropImage = () => {
+    if (!croppedAreaPixels) return
+    cropImage(file.data, croppedAreaPixels.width, croppedAreaPixels.height, croppedAreaPixels.x, croppedAreaPixels.y, (imgUri) => {
+      let encoded = imgUri.toString().replace(/^data:(.*,)?/, '');
+      if ((encoded.length % 4) > 0) {
+        encoded += '='.repeat(4 - (encoded.length % 4));
+      }
+      const blob = b64toBlob(encoded, "image/jpeg");
+      const fileInfo = {
+        type: blob.type,
+        size: blob.size,
+        blob: blob,
+        data: imgUri
+      };
+      console.log("fileInfo", fileInfo);
+      setFile(fileInfo);
     });
   }
 
@@ -135,9 +183,22 @@ export function WebcamWrapper({ setFile, file }) {
     </div>
   ) : (
     <div className="is-relative has-text-centered">
-      <div style={{ maxWidth: 640, maxHeight: 480, margin: "auto", background: `url(${file.data}) no-repeat center`, backgroundSize: "cover" }}>
-        <div style={{ paddingBottom: "75%" }} />
+      <div style={{ maxWidth: 640, maxHeight: 480, margin: "auto" }}>
+        <div style={{ paddingBottom: "75%" }}>
+          <Cropper
+            image={file.data}
+            crop={crop}
+            zoom={zoom}
+            aspect={4 / 3}
+            onCropChange={setCrop}
+            onCropComplete={onCropComplete}
+            onZoomChange={setZoom}
+          />
+        </div>
       </div>
+
+      
+
       {!file.uploaded &&
         <SaveButton file={file} />
       }
@@ -145,6 +206,23 @@ export function WebcamWrapper({ setFile, file }) {
         icon="delete"
         handleClick={clearImage}
       />
+      {croppedAreaPixels &&
+        <Button
+          renderAs="a"
+          rounded
+          style={{
+            position: "absolute",
+            bottom: "1rem",
+            left: "calc(50% + 2.5rem)",
+            height: "3rem",
+            background: "rgba(46, 49, 54, 0.6)",
+            border: 0
+          }}
+          onClick={doCropImage}
+        >
+          <span>CROP</span>
+        </Button>
+      }
     </div>
   )
 }
