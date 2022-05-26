afa16f414972fcad96684f7fa0283d2db4f898cf
niteshudhani <95778087+niteshudhani@users.noreply.github.com>
2022-05-26 02:57:20 -0400

    MOD-174: SDK Cleaning up

--- sdk/example/modclub_provider/src/modclub_provider_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { modclub_provider } from "../../declarations/modclub_provider";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with modclub_provider actor, calling the greet method
-  const greeting = await modclub_provider.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
