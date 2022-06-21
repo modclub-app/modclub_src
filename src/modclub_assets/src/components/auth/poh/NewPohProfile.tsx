5845da7af2328b24e54dd64017a615c4b970f4e0
Taylor Chance <taylor@puffdigital.com>
2022-06-21 14:04:01 -0400

    redirectUri

--- src/modclub_assets/src/components/auth/poh/NewPohProfile.tsx
+++ src/modclub_assets/src/components/auth/poh/NewPohProfile.tsx
@@ -16,23 +16,31 @@ import UserVideo from "./UserVideo";
 import UserPhrases from "./UserPhrases";
 import { verifyUserHumanity, retrieveChallengesForUser } from '../../../utils/api';
 
-const Confirmation = () => {
+const Confirmation = ({ redirect_uri }) => {
   return (
     <div className="has-text-centered">
       <Heading subtitle textAlign="center">
         Thank you for submitting.
       </Heading>
       <p>Your verification is in progress, please check back soon.</p>
-      <Link to="/app" className="button is-large is-primary mt-5">
-        Back to MODCLUB
-      </Link>
+
+      {redirect_uri ? (
+        <a href={redirect_uri} className="button is-large is-primary mt-5">
+          Back to {redirect_uri}
+        </a>
+      ) : (
+        <Link to="/app" className="button is-large is-primary mt-5">
+          Back to MODCLUB
+        </Link>
+      )}
     </div>
   )
 };
 
 export default function NewPohProfile({ match }) {
   const { search } = useLocation();
-  const URLtoken = new URLSearchParams(search).get("token");
+  const params = new URLSearchParams(search);
+  const URLtoken = params.get("token");
   const { isAuthenticated, isAuthReady, user } = useAuth();
   const history = useHistory();
   const [loading, setLoading] = useState<boolean>(true);
@@ -40,12 +48,15 @@ export default function NewPohProfile({ match }) {
   const [currentStep, setCurrentStep] = useState<string>('')
   // const [hasInitialCall, setHasInitialCall] = useState<boolean>(false);
   const [noToken, setNoToken] = useState<boolean>(false);
+  const [redirectUri, setRedirectUri] = useState<string | null>(null);
 
   const initialCall = async (token) => {
     if (!token) {
       setNoToken(true);
       return
     }
+
+    setRedirectUri(params.get("redirect_uri"))
 
     const challenges = await retrieveChallengesForUser(token);
     setLoading(false);
@@ -119,7 +130,9 @@ export default function NewPohProfile({ match }) {
                   <Route path={`${match.path}/:challenge-user-audio`}>
                     {steps && <UserPhrases steps={steps} />}
                   </Route>
-                  <Route path={`${match.path}/:confirm`} component={Confirmation}/>
+                  <Route path={`${match.path}/:confirm`}>
+                    <Confirmation redirect_uri={redirectUri} />
+                  </Route>
                 </Switch>
               </Card.Content>
             </Card>
