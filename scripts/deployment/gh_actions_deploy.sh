23f9d5a2e0bc683313fc3d79d63ed47757af3a89
Shallwe <xwdeng@hotmail.com>
2023-10-04 22:24:37 -0400

    Fix: Catch deployment error (#414)
    To catch error: Error: Failed while trying to deploy canisters.
    Caused by: Failed while trying to deploy canisters.

--- scripts/deployment/gh_actions_deploy.sh
+++ scripts/deployment/gh_actions_deploy.sh
@@ -8,9 +8,13 @@ source "${current_dir}/deployment_utils.sh"
 deploy_canisters $DEV_ENV ic $OLD_MODCLUB_INSTANCE 2>&1 | tee deploy_logs.txt
 
 if grep -q "Do you want to proceed? yes/No" deploy_logs.txt; then
-    echo "Detected the prompt in deploy_logs.txt. Failing the script."
+    echo "Detected the 'Do you want to proceed?' prompt in deploy_logs.txt. Failing the script."
+    exit 1
+elif grep -q "Error: Failed while trying to deploy canisters" deploy_logs.txt; then
+    echo "Detected the 'Failed while trying to deploy canisters' error in deploy_logs.txt. Failing the script."
     exit 1
 else
-    echo "No prompt detected in deploy_logs.txt. All is well."
+    echo "All good."
     exit 0
 fi
+
