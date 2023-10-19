532ce5046ff9b2c00625149710b4d2d6f6bddce0
rgovindji <rgovindji@gmail.com>
2023-10-19 12:25:44 -0400

    Latest declarations and changed button from beta 1.0 to launch app

--- src/declarations/modclub/index.js
+++ src/declarations/modclub/index.js
@@ -10,7 +10,8 @@ export { idlFactory } from "./modclub.did.js";
  * beginning in dfx 0.15.0
  */
 export const canisterId =
-  process.env.CANISTER_ID_MODCLUB || process.env.MODCLUB_CANISTER_ID;
+  process.env.CANISTER_ID_MODCLUB ||
+  process.env.MODCLUB_CANISTER_ID;
 
 export const createActor = (canisterId, options = {}) => {
   const agent = options.agent || new HttpAgent({ ...options.agentOptions });
