8b22de82c597b226b919e37e8f39e2aee8d95f80
Shallwe <xwdeng@hotmail.com>
2023-12-15 15:46:35 -0500

    MOD-607, MOD-629 Enhance slack msg in github actions (#556)
    - Add backup related msg
    - Use github PR titles for what's new
    - Move what's new to when deployment start
    
    
    
    
    
    
    
    
    
    
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    - Chore: Updated GitHub Actions workflow files and deployment
    notification scripts for improved maintainability.
    - New Feature: Enhanced Slack notifications with backup-related messages
    and more detailed "What's new" information, fetched directly from GitHub
    PR titles.
    - Refactor: Streamlined the backup process by updating variable names
    and removing duplicate code.
    - Chore: Commented out the check for `main` branch, pending further
    review.
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

--- scripts/deployment/gh_deployment_notify_end.cjs
+++ scripts/deployment/gh_deployment_notify_end.cjs
@@ -10,23 +10,13 @@ const COMMIT_SHA = process.env.COMMIT_SHA;
 const LAST_DEPLOYMENT_TAG = process.env.LAST_DEPLOYMENT_TAG;
 const DEPLOYMENT_TAG = process.env.DEPLOYMENT_TAG;
 
-let WHATS_NEW_SINCE_LAST_DEP = process.env.WHATS_NEW_SINCE_LAST_DEP;
-
-// https://api.slack.com/methods/chat.postMessage#:~:text=Ideally%2C%20messages%20should%20be%20short,containing%20more%20than%2040%2C000%20characters.
-SLACK_MSG_LEN_LIMIT = 4000;
-if (WHATS_NEW_SINCE_LAST_DEP.length >= SLACK_MSG_LEN_LIMIT) {
-  WHATS_NEW_SINCE_LAST_DEP =
-    WHATS_NEW_SINCE_LAST_DEP.substring(0, SLACK_MSG_LEN_LIMIT - 3) + "...";
-  // TODO: could use chatgpt api to summarize the content instead of truncate the value
-}
-
 const template = {
   blocks: [
     {
       type: "section",
       text: {
         type: "mrkdwn",
-        text: `*[${DEV_ENV} env] Deployment(${DEPLOYMENT_TAG}) ends.* \n\n Commit: ${COMMIT_SHA}\n<https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}|Check workflow>\n<https://github.com/${GITHUB_REPOSITORY}/pull/${PR_NUMBER}|View PR #${PR_NUMBER}>\n\n*What's new in the HEAD since last deployment(${LAST_DEPLOYMENT_TAG})*:\n${WHATS_NEW_SINCE_LAST_DEP}\n`,
+        text: `*[${DEV_ENV} env] Deployment(${DEPLOYMENT_TAG}) ends.* \n\n Commit: ${COMMIT_SHA}\n<https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}|Check workflow>\n<https://github.com/${GITHUB_REPOSITORY}/pull/${PR_NUMBER}|View PR #${PR_NUMBER}>\n`,
       },
     },
   ],
--- scripts/deployment/gh_deployment_notify_starts.cjs
+++ scripts/deployment/gh_deployment_notify_starts.cjs
@@ -2,11 +2,23 @@ const { notify_slack } = require("./slack_util.cjs");
 
 const DEPLOYMENT_NOTIFICATION_PATH = process.env.DEPLOYMENT_NOTIFICATION_PATH;
 const DEV_ENV = process.env.DEV_ENV;
+let WHATS_NEW_SINCE_LAST_DEP = process.env.WHATS_NEW_SINCE_LAST_DEP;
 
+// https://api.slack.com/methods/chat.postMessage#:~:text=Ideally%2C%20messages%20should%20be%20short,containing%20more%20than%2040%2C000%20characters.
+SLACK_MSG_LEN_LIMIT = 400;
+if (WHATS_NEW_SINCE_LAST_DEP.length >= SLACK_MSG_LEN_LIMIT) {
+  WHATS_NEW_SINCE_LAST_DEP =
+    WHATS_NEW_SINCE_LAST_DEP.substring(0, SLACK_MSG_LEN_LIMIT - 3) + "...";
+  // TODO: could use chatgpt api to summarize the content instead of truncate the value
+}
 const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
 const GITHUB_RUN_ID = process.env.GITHUB_RUN_ID;
 const PR_NUMBER = process.env.PR_NUMBER;
 const COMMIT_SHA = process.env.COMMIT_SHA;
+const backupId_1 = process.env.BACKUP_ID_1;
+const backupFieldName_1 = process.env.BACKUP_FIELDNAME_1;
+const LAST_DEPLOYMENT_TAG = process.env.LAST_DEPLOYMENT_TAG;
+const DEPLOYMENT_TAG = process.env.DEPLOYMENT_TAG;
 
 const template = {
   blocks: [
@@ -14,7 +26,7 @@ const template = {
       type: "section",
       text: {
         type: "mrkdwn",
-        text: `*[${DEV_ENV} env] Deployment starts.* \n\n Commit: ${COMMIT_SHA}\n<https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}|Check workflow>\n<https://github.com/${GITHUB_REPOSITORY}/pull/${PR_NUMBER}|View PR #${PR_NUMBER}>`,
+        text: `*[${DEV_ENV}] Deployment(${DEPLOYMENT_TAG}) starts.* \n\n Commit: ${COMMIT_SHA}\n<https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}|Check workflow>\n<https://github.com/${GITHUB_REPOSITORY}/pull/${PR_NUMBER}|View PR #${PR_NUMBER}> \n\n *What's new in the HEAD since last deployment(${LAST_DEPLOYMENT_TAG})* \n ${WHATS_NEW_SINCE_LAST_DEP} \n\n Backups:[${backupId_1} - ${backupFieldName_1}] \n`,
       },
     },
   ],
