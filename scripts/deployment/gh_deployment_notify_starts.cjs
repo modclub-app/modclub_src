50f086e401b345b251132ebb7cf18e5dbb0f437c
Shallwe <xwdeng@hotmail.com>
2023-12-23 09:31:12 -0500

    re-format slack msg (#562)
    - Formatted deployment message to : `PR #XXX Title: <Title of PR>
    Commit: <Hash> of commit`
    - For dev env, skip local commit messages
    
    
    
    
    
    
    
    
    
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    - Chore: Updated the deployment notification process in both dev and qa
    workflows. The new implementation retrieves commit titles and hashes,
    filters them based on a specific pattern, and sends them to Slack.
    - Refactor: Modified the logic for exporting commit information. If the
    number of commits is less than or equal to 20, all are exported.
    Otherwise, only the first and last 10 are exported with an ellipsis in
    between.
    - Chore: Increased the maximum allowed length of Slack messages from 400
    to 2000 characters in the `gh_deployment_notify_starts.cjs` script.
    - Chore: Commented out certain steps related to deployment, tagging,
    restoration, and Slack notification in the `deploy_dev.yml` file.
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

--- scripts/deployment/gh_deployment_notify_starts.cjs
+++ scripts/deployment/gh_deployment_notify_starts.cjs
@@ -5,7 +5,7 @@ const DEV_ENV = process.env.DEV_ENV;
 let WHATS_NEW_SINCE_LAST_DEP = process.env.WHATS_NEW_SINCE_LAST_DEP;
 
 // https://api.slack.com/methods/chat.postMessage#:~:text=Ideally%2C%20messages%20should%20be%20short,containing%20more%20than%2040%2C000%20characters.
-SLACK_MSG_LEN_LIMIT = 400;
+SLACK_MSG_LEN_LIMIT = 2000;
 if (WHATS_NEW_SINCE_LAST_DEP.length >= SLACK_MSG_LEN_LIMIT) {
   WHATS_NEW_SINCE_LAST_DEP =
     WHATS_NEW_SINCE_LAST_DEP.substring(0, SLACK_MSG_LEN_LIMIT - 3) + "...";
