28189faf6e405f6107265c8eaeef0319ad8d32e1
Shallwe <xwdeng@hotmail.com>
2024-01-12 10:51:46 -0500

    Catch Error "Failed while trying to generate type declarations" (#579)

--- scripts/deployment/gh_actions_deploy.sh
+++ scripts/deployment/gh_actions_deploy.sh
@@ -16,6 +16,9 @@ if grep -q "Do you want to proceed? yes/No" deploy_logs.txt; then
 elif grep -q "Error: Failed while trying to deploy canisters" deploy_logs.txt; then
     echo "Detected the 'Failed while trying to deploy canisters' error in deploy_logs.txt. Failing the script."
     exit 1
+elif grep -q "Error: Failed while trying to generate type declarations" deploy_logs.txt; then
+    echo "Detected the 'Error: Failed while trying to generate type declarations' error in deploy_logs.txt. Failing the script."
+    exit 1
 else
     echo "All good."
     exit 0
