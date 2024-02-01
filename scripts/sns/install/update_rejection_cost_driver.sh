fb8e6d25980ff32d15e6924d3bcaad949051486c
umut yorulmaz <82664734+umutyorulmaz@users.noreply.github.com>
2024-02-01 08:58:36 -0500

    if statement removed

--- scripts/sns/install/update_rejection_cost_driver.sh
+++ scripts/sns/install/update_rejection_cost_driver.sh
@@ -8,9 +8,4 @@ cd  SCRIPT_DIR
 
 . ./constants.sh normal
 
-if [ -f "./sns_canister_ids.json" ]
-then
-    ./update_rejection_cost_logic.sh
-else
-    ./update_rejection_cost_logic.sh
-fi
+./update_rejection_cost_logic.sh
