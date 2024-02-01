ba4a17284e880ce2a617eff74e77e8399996e975
rgovindji <rgovindji@gmail.com>
2024-02-01 23:08:32 +0400

    Updated proposal script with new rejection cost

--- scripts/sns/install/update_rejection_cost_logic.sh
+++ scripts/sns/install/update_rejection_cost_logic.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-# This script is used to create an SNS proposal for increasing the cost of proposal rejection to 200 MOD
-# This script is called by update_rejection_cost_driver.sh
-# For detailed explanation please see the GitHub Repo: https://github.com/dfinity/sns-testing/tree/main
+# This script is used to create an SNS proposal for increasing the cost of proposal rejection to 2000 MOD
 
 
 set -euo pipefail
@@ -28,9 +26,9 @@ quill sns  \
 --pem-file "${PEM_FILE}" \
 make-proposal --proposal \
 "(record { \
-       title = \"Increase Proposal Rejection Cost\"; \
-       url = \"https://example.com/\"; \
-       summary = \"Proposal to increase the rejection cost to 200 MOD\"; \
+       title = \"Increase proposal rejection cost\"; \
+       url = \"https://modclub.ai.com/\"; \
+       summary = \"Proposal to increase the rejection cost to 2000 MOD in order to reduce the risk of spam proposals.\"; \
        action = opt variant { \
            ManageNervousSystemParameters = record { \
               voting_rewards_parameters = opt record {
@@ -48,7 +46,7 @@ make-proposal --proposal \
     max_neuron_age_for_age_bonus = opt (63_115_200 : nat64);
     initial_voting_period_seconds = opt (345_600 : nat64);
     neuron_minimum_dissolve_delay_to_vote_seconds = opt (2_629_800 : nat64);
-    reject_cost_e8s = opt (20_000_000_000 : nat64);
+    reject_cost_e8s = opt (200_000_000_000 : nat64);
     max_proposals_to_keep_per_action = opt (100 : nat32);
     wait_for_quiet_deadline_increase_seconds = opt (86_400 : nat64);
     max_number_of_neurons = opt (200_000 : nat64);
@@ -66,5 +64,4 @@ make-proposal --proposal \
 })" \
 "${DEVELOPER_NEURON_ID}" > msg.json
 quill send \
---insecure-local-dev-mode \
---yes msg.json | grep -v "new_canister_wasm"
+--yes msg.json
