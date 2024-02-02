6020e6cb84f4929a746005473dde90a2a2c11430
Justin Deng <justin.dengcn@gmail.com>
2024-02-01 22:21:32 -0500

    Clean usergeek

--- src/decideid_assets/src/App.tsx
+++ src/decideid_assets/src/App.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import { useEffect } from "react";
-import { Usergeek } from "usergeek-ic-js";
 import Header from "./components/header/Header";
 
 import {
@@ -13,17 +11,6 @@ import Authed from "./components/routers/Authed";
 
 export default function App() {
   const history = useHistory();
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      Usergeek.init({ apiKey: "01C001B685B0F32CFCCEEBAA4C97E390" });
-    } else {
-      Usergeek.init({
-        apiKey: "012701B7CC044FBD096F55C8DAA5413E",
-        host: "https://ljyte-qiaaa-aaaah-qaiva-cai.ic0.app",
-      });
-    }
-  }, []);
 
   return (
     <Router history={history}>
