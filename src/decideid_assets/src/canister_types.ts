9b66bfe36f7e0aecc574cfc084c94ad42d2af3b7
Justin Deng <justin.dengcn@gmail.com>
2024-02-01 22:51:06 -0500

    remove autogen

--- src/decideid_assets/src/canister_types.ts
+++
@@ -1,7 +0,0 @@
-// This file is auto generated from scripts/build/gen_files_by_env.cjs
-// Current DEV_ENV=qa
-
-import * as modclub_types from "../../declarations/modclub_qa/modclub_qa.did";
-import * as wallet_types from "../../declarations/wallet_qa/wallet_qa.did";
-
-export { modclub_types, wallet_types };
