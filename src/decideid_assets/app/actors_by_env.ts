036d7fe57091be134771203e5b887ebf3c158ff0
Vitaliy Klepka <vitaly.klepka@gmail.com>
2024-02-19 12:39:58 +0100

    cleanup

--- src/decideid_assets/app/actors_by_env.ts
+++
@@ -1,7 +0,0 @@
-// This file is auto generated from scripts/build/gen_files_by_env.cjs
-// Current DEV_ENV=qa
-
-import * as modclub_actor from "../../declarations/modclub_qa";
-import * as decidedid_actor from "../../declarations/decideid_qa";
-
-export { modclub_actor, decidedid_actor };
