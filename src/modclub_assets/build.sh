aa698f7bdfb94c7571f725882f6144f747714cfb
Shallwe <xwdeng@hotmail.com>
2024-07-02 16:37:17 -0400

    MOD-1071 use dfx deploy with gzip option (#88)
    * use dfx deploy with gzip
    
    * .
    
    * fix network
    
    * set -x
    
    * fix wrong number of argument
    
    * remove debug

--- src/modclub_assets/build.sh
+++ src/modclub_assets/build.sh
@@ -41,5 +41,4 @@ cp ./target/wasm32-unknown-unknown/release/modclub_assets.wasm ./modclub_assets_
 ic-wasm "./target/wasm32-unknown-unknown/release/modclub_assets.wasm" -o "./modclub_assets.wasm" shrink &&
 ic-wasm modclub_assets.wasm -o modclub_assets.wasm metadata candid:service -f modclub_assets.did -v public &&
 # indicate support for certificate version 1 and 2 in the canister metadata
-ic-wasm modclub_assets.wasm -o modclub_assets.wasm metadata supported_certificate_versions -d "1,2" -v public &&
-gzip --no-name --force "modclub_assets.wasm"
+ic-wasm modclub_assets.wasm -o modclub_assets.wasm metadata supported_certificate_versions -d "1,2" -v public
