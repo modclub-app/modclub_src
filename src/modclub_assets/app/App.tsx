d6e0a0355a65f0db83098dbc3ddc47c5a9c7eeab
rgovindji <rgovindji@gmail.com>
2024-09-04 11:59:23 -0400

    .

--- src/modclub_assets/app/App.tsx
+++ src/modclub_assets/app/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { useEffect } from "react";
-import { Usergeek } from "usergeek-ic-js";
 import "./bulma.css";
 import "./App.scss";
 import "./Responsive.scss";
@@ -20,17 +19,6 @@ import { ProfileProvider } from "./contexts/profile";
 
 export default function App() {
   const history = useHistory();
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      Usergeek.init({ apiKey: "01C001B685B0F32CFCCEEBAA4C97E390" });
-    } else {
-      Usergeek.init({
-        apiKey: "012701B7CC044FBD096F55C8DAA5413E",
-        host: "https://ljyte-qiaaa-aaaah-qaiva-cai.ic0.app",
-      });
-    }
-  }, []);
 
   return (
     <StateProvider>
