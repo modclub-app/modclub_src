631430f0ae3a8cab32c764f8057075863086f0e8
Александр Вендер <alex_vender@Aleksandrs-MacBook-Pro.local>
2024-09-05 16:25:51 -0400

    MOD-999: Remove new_frontend

--- new_frontend/src/components/uikit/Icon/JewelryIcon/index.tsx
+++
@@ -1,5 +0,0 @@
-import { withSvgWrap } from "../SvgWrap";
-
-export const JewelryIcon = withSvgWrap(
-  "M109 45L74.6364 45L55 90L89.3636 90L109 45Z M145 -2.40413e-06L110 -8.74228e-07L90 45L125 45L145 -2.40413e-06Z M4.79505e-06 45L35 45L55 90L20 90L4.79505e-06 45Z"
-);
