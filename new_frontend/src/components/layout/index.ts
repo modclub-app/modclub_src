94a2e41b1bf987d87a56a971b040754bea252832
AVender <145117126+aleksandr-vender@users.noreply.github.com>
2024-09-12 16:15:36 -0400

    MOD-999: Remove new_frontend (#105)
    Co-authored-by: Александр Вендер <alex_vender@Aleksandrs-MacBook-Pro.local>

--- new_frontend/src/components/layout/index.ts
+++
@@ -1,5 +0,0 @@
-export { Grid } from "./Grid";
-export { Container } from "./Container";
-export { Navigation } from "./Navigation";
-export { Footer } from "./Footer";
-export { BgWrap } from "./BgWrap";
